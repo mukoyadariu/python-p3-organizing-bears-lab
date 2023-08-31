fb28dc0437db1a60cb2e8801ef3458263779ec14
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-31 08:57:08 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+    ('Baloo', 10, 'Male', 'Brown', 'Easygoing', 1),
+    ('Yogi', 7, 'Male', 'Brown', 'Mischievous', 1),
+    -- More insert statements
+    (NULL, 3, 'Female', 'White', 'Playful', 1);
